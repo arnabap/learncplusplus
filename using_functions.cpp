2db8396cb3b0da5b6ac49b2985a021d5c723e93c
apurkaya <45953266+apurkaya@users.noreply.github.com>
2020-11-12 23:43:58 -0500

    functions added

---
+++ using_functions.cpp
@@ -0,0 +1,91 @@
+/******************************************************************************
+
+Define a function PrintFeetInchShort, with int parameters numFeet and numInches,
+that prints using ' and " shorthand. End with a newline. 
+Ex: PrintFeetInchShort(5, 8) prints: 5' 8"
+*******************************************************************************/
+
+// #include<iostream>
+// using namespace std;
+
+// void PrintFeetInchShort(int numFeet , int numInches){
+
+// cout<< numFeet<<"' "<<numInches<<"\""<<endl;
+// }
+
+// int main(){
+
+//    int userFeet;
+//    int userInches;
+
+//    cin >> userFeet;
+//    cin >> userInches;
+
+//    PrintFeetInchShort(userFeet, userInches); 
+
+//    return 0;
+// }
+
+/******************************************************************************
+Assign to maxSum the max of (numA, numB) PLUS the max of (numY, numZ). 
+
+*******************************************************************************/
+// #include<iostream>
+// using namespace std;
+
+// double maxfind(double num1, double num2){
+// double max;
+// if(num1>num2){
+//     max=num1;
+// }
+// else
+// {
+//     max=num2;
+// }
+// return max;
+// }
+
+
+
+// int main() {
+//    double numA;
+//    double numB;
+//    double numY;
+//    double numZ;
+//    double maxSum;
+
+//    cin >> numA;
+//    cin >> numB;
+//    cin >> numY;
+//    cin >> numZ;
+
+
+//    maxSum=maxfind(numA,numB)+maxfind(numY,numZ);
+
+//    cout << "maxSum is: " << maxSum << endl;
+
+//    return 0;
+// }
+
+/******************************************************************************
+One lap around a standard high-school running track is exactly 0.25 miles. 
+Write a program that takes a number of miles as input, and outputs the number of laps.
+Output each floating-point value with two digits after the decimal point.
+*******************************************************************************/
+// #include<iostream>
+// #include<iomanip>
+// using namespace std;
+
+// double mtol(double miles){
+// return miles/0.25;
+
+// }
+
+// int main(){
+//     double miles;
+//     cin>>miles;
+//     cout<<fixed<<setprecision(2);
+//     cout<<"Laps"<<mtol(miles);
+
+
+// }
